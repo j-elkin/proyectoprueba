81af6144e21bc19284d85fa36e8c47d5be9a1a61
John Elkin Rendón Romero <j.erendonr@gmail.com>
2015-02-19 22:22:04 -0500

    proyecto animales
    Primera versión del proyecto de base de datos animales.

---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+	<head>
+		<title>Menú</title>
+		<link href="estiloParaMenu.css" rel="stylesheet" type="text/css" />
+	</head>
+
+	<body BACKGROUND="img_fondo/animales_clasificados.jpg">
+		<CENTER>
+				<h1><FONT FACE="Arial,Helvetica,Times">Animales</FONT></h1>
+		</CENTER>
+		<div id="menu">
+			<ul class="est1">
+				<li><a href="insertar_animal.php">Insertar Animal</a></li>
+				<li><a href="consultar_animales.php">Consultar Animal</a></li>
+				<li><a href="borrar_registro.php">Eliminar Animal</a></li>
+				<li><a href="consultaPorMapaSensitivo.php">Consultar Animales por Ubicación</a></li>
+				<li><a href="nosotros.php">Nosotros</a></li>
+			</ul>
+		</div>
+	</body>
+</html>
